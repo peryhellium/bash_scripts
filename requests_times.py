ad157213414c9ab0ad4eebd62280937796ffd474
Majkel <michal.krakow7@gmail.com>
2023-01-19 12:02:54 +0100

    requests script added

---
+++ requests_times.py
@@ -0,0 +1,43 @@
+# Ask the user for the name of the file
+filename = raw_input("Enter the name of the file to process: ")
+
+# Set counters to 0
+up_to_1 = 0
+between_1_5 = 0
+between_5_10 = 0
+between_10_20 = 0
+between_20_30 = 0
+over_30 = 0
+
+# Open the file
+with open(filename, "r") as f:
+  # Read each line in the file
+  for line in f:
+    # Split the line into columns
+    columns = line.split()
+    # Check if the line has at least 12 columns
+    if len(columns) >= 12:
+      # Extract the time from the 12th column
+      time = float(columns[11])
+      # Check which category the time falls into and increment the appropriate counter
+      if time < 1000:
+        up_to_1 += 1
+      elif 1000 <= time < 5000:
+        between_1_5 += 1
+      elif 5000 <= time < 10000:
+        between_5_10 += 1
+      elif 10000 <= time < 20000:
+        between_10_20 += 1
+      elif 20000 <= time < 30000:
+        between_20_30 += 1
+      else:
+        over_30 += 1
+
+# Print the results
+print("Up to 1 second: {}".format(up_to_1))
+print("Between 1 second and 5 seconds: {}".format(between_1_5))
+print("Between 5 seconds and 10 seconds: {}".format(between_5_10))
+print("Between 10 seconds and 20 seconds: {}".format(between_10_20))
+print("Between 20 seconds and 30 seconds: {}".format(between_20_30))
+print("Over 30 seconds: {}".format(over_30))
+
